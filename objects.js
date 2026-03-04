81c75b76dac993f64606c1cd3082ed98d76ad794
Paul Niemczyk <mrpndev@gmail.com>
2026-03-03 20:47:04 -0500

    Objects take home

--- objects.js
+++ objects.js
@@ -103,6 +103,13 @@ console.log(newArray.filter(i => i))
 student.gpa = student.grades.reduce((sum, num) => sum + num, 0) / student.grades.length
 console.log(student.gpa)
 
+/* 
+	? What is reduce?
+	* it takes an accumulator (sum)
+	* it also takes an initial value (0)
+	* as it loops, it keeps track of the total for us in the accumulator
+*/
+
 // Using forEach
 let total = 0
 student.grades.forEach(grade => total += grade)
@@ -111,3 +118,104 @@ student.gpaForEach = total / student.grades.length
 
 console.log(student.gpaForEach)
 
+/* 
+	? Learn Objects by creating authentication service
+*/
+
+let payload = {
+	email: "paul@codecademy.com",
+	password: "dbLocal"
+}
+
+let db = [
+	{ email: "paul@codecademy.com", password: "dbLocal"},
+	{ email: "scott@gmail.com", password: "Scotti3"},
+	{ email: "arush@google.com", password: "password"},
+	{ email: "rus@cyberexpert.com", password: "lakdh&)ldkx-dakhjlf"}
+]
+
+// Function is used to encapsulate reusable code
+function authenticate(database, request) {
+
+	// let foundUser = []
+	// database.forEach(usr => {
+	// 	if (usr.email === request.email) {
+	// 		foundUser.push(usr)
+	// 	}
+	// })
+
+	let foundUser = database.filter(usr => usr.email === request.email)
+	console.log(foundUser)
+	if (foundUser.length) {
+		if (foundUser[0].password === request.password) {
+			console.log("Logged in")
+		} else {
+			console.log("Invalid password")
+		}
+	} else {
+		console.log("No user, go away")
+	}
+}
+
+authenticate(db, payload)
+
+/* 
+	? Array Copy Shenanigans
+*/
+
+let arr1 = ["Gary", "Paul", "Jake"]
+let arr2 = arr1
+console.log(arr1, arr2)
+
+let var1 = "Paul"
+let var2 = var1
+console.log(var1, var2)
+var2 = "Kiara"
+console.log(var1, var2)
+
+arr2[0] = "Arthur"
+console.log(arr1, arr2)
+// This modifies both arrays because reference values are by pointer
+// shallow copy
+let arr3 = arr1.map(i => i) // deep copy
+arr3[0] = "Potato"
+console.log(arr1, arr3)
+
+// ? Take Home Challenge
+
+let album = {
+  id: 55,
+  artist: "The Midnight",
+  likes: 4200,
+  albumInfo: {
+    album1: {
+      numberOfSongs: 3,
+      songInfo: [
+        { track: 1, songName: "Days of Thunder" },
+        { track: 2, songName: "Los Angeles" },
+        { track: 3, songName: "Sunset" }
+      ]
+    },
+    album2: {
+      numberOfSongs: 2,
+      songInfo: [
+        { track: 1, songName: "Neon Lights" },
+        { track: 2, songName: "City Dreams" }
+      ]
+    }
+  },
+  isAvailable: true
+}
+
+/* 
+	* print the second song name from album1
+	* add album3 inside albumInfo
+	* album3 should contain:
+		* numberOfSongs
+		* songInfo (array of 2 song objects with track and songName properties)
+		* fabricate their values to whatever you wish
+
+	! HARD MODE: add a property called totalAlbums which should be total number of albums.
+	* hint: use Object.keys() and something else
+		
+*/
